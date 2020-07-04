0edfcebd72c56b4756b0c9f9c6e09ed3b750749b
kurima reiji <kurimareiji@kurimail.com>
2020-07-04 20:53:53 +0900

    yahoo

---
+++ study1.js
@@ -0,0 +1,141 @@
+const core = require("@actions/core");
+const puppeteer = require("puppeteer-core");
+const io = require("@actions/io");
+const os = require("os");
+const path = require("path");
+
+const errHandler = (err) => console.log(err);
+const browserPath = "/usr/bin/google-chrome";
+const date = `2020-07-03`;
+const targetDir = `${process.env.GITHUB_WORKSPACE}/json/`;
+
+const scraper_get_targets = () => {
+  return Array.from(document.querySelectorAll("#gm_card a")).map((a) => a.href.replace("index", "text"));
+};
+
+const scraper = () => {
+  const pathname = location.pathname.split("/").slice(-2).join("_");
+
+  const outfile = `yahoo_${pathname}.json`;
+
+  const output_json = (data, outfile) => {
+    const $json = JSON.stringify(data, null, 2);
+    const bb = new Blob([$json]);
+
+    document.body.insertAdjacentHTML('beforeend', `<a id="jsDownloader">Download</a>`);
+
+    const dl = document.getElementById("jsDownloader");
+    dl.href = window.URL.createObjectURL(bb);
+    dl.setAttribute("download", outfile);
+    dl.click();
+    document.body.removeChild(dl);
+  };
+
+  const inning_header = (header) => {
+    const data = ["inning", "detail"]
+      .map((item) => {
+        return {
+          item,
+          el: header.querySelector(`.bb-liveText__${item}`),
+        };
+      })
+      .filter((obj) => obj.el)
+      .map((obj) => {
+        return {
+          item: obj.item,
+          text: obj.el.textContent.trim(),
+        }
+      })
+      .reduce((acc, cur) => {
+        acc[cur.item] = cur.text;
+        return acc;
+      }, {})
+      ;
+    data.id = header.id;
+    return data;
+  };
+
+  const get_data = (lis) => {
+    return lis.map((li) => {
+      const data = ["order", "player", "state", "summary"].map((item) => {
+        return {
+          item,
+          elements: Array.from(li.querySelectorAll(`.bb-liveText__${item}`)),
+        }
+      })
+        .filter((obj) => obj.elements.length > 0)
+        .map((obj) => {
+          return {
+            item: obj.item,
+            elements: obj.elements.map((el) => el.textContent.split("\n").map((line) => line.trim()).filter((s) => s).join(" ")),
+          }
+        })
+        .reduce((acc, cur) => {
+          acc[cur.item] = cur.elements;
+          return acc;
+        }, {})
+        ;
+
+
+      return data;
+    });
+  };
+
+  const history = Array.from(document.querySelectorAll("section.bb-liveText"))
+    .map((sec) => {
+      const header = inning_header(sec.querySelector("header"));
+      const plays = get_data(Array.from(sec.querySelectorAll(".bb-liveText__item")));
+      return {
+        header,
+        plays,
+      }
+    })
+    ;
+
+  const retrievedAt = (() => {
+    const d = new Date();
+    d.setMinutes(d.getMinutes() + 540);
+    return d.toISOString().slice(0, 19);
+  })();
+
+  const data = {
+    url: location.href,
+    title: document.querySelector(".bb-head01__title").textContent.split("\n").map((s) => s.trim()).filter((s) => s).join(" "),
+    retrievedAt,
+    history
+  };
+
+  output_json(data, outfile);
+  return data;
+};
+
+
+(async () => {
+
+  await io.mkdirP(targetDir);
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: browserPath,
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1200, height: 800 });
+  await page._client.send('Page.setDownloadBehavior',
+    {
+      behavior: 'allow',
+      downloadPath: './myAwesomeDownloadFolder'
+    }
+  );
+  await page.goto(`https://baseball.yahoo.co.jp/npb/schedule/?date=${date}`);
+  await page.waitForSelector("#gm_card");
+  const targets = await page.evaluate(scraper_get_targets);
+
+  for (let target of targets) {
+    await page.goto(target);
+    await page.waitForSelector("section.bb-liveText");
+    await page.evaluate(scraper);
+  };
+
+  await page.waitFor(1200);
+  await browser.close();
+})();
